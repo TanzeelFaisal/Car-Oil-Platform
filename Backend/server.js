3d8fbfcc30b9f5c384816636f5dd53bb880f5c5f
TanzeelFaisal <132434163+TanzeelFaisal@users.noreply.github.com>
2024-02-25 13:59:00 +0500

    Revert "unnecessary"
    This reverts commit 740e0de5d086ef8603ed166a80dd4491fb66ce86.

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const port = 3001;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3001'
+    origin: 'http://localhost:3000'
 }))
 
 const pool = mysql.createPool(dbConfig);
